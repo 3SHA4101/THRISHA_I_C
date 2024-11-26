48646861ccd304c7924d84f32cc865ea12e41d75
3SHA4101 <thrisha4101@gmail.com>
2024-11-26 14:08:14 +0530

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+  float a,b,sum;
+  printf("enter the values of a and b\n");
+  scanf("%f%f",&a,&b);
+  sum=a+b;
+  printf("(by Thrisha)the sum of a and b is %f",sum);
+  return 0;
+}
