dc296b66b25f454acae049e8a4e119b8880387fa
3SHA4101 <thrisha4101@gmail.com>
2024-12-04 08:22:57 +0530

    Create binarysearch.c

---
+++ binarysearch.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int low,high,mid,key,n,i;
+    printf("enter the numbers of elements");
+    scanf("%d",&n);
+    printf("enter %d numbers in ascending order/n");
+for(i=0;i<n;i++)
+scanf(%d",&a[i];
+printf("enter the number to be search");
+scan("%d,&key);
+low=0;
+high=n-1;
+while(low<=high)
+{
+ mid=(low+high)/2;
+        if(key==a[mid])
+        {
+            printf("key %d is found at the position %d /n",key,mid+1);
+            return;
+        }
+        else if((key>a[mid])>0)
+        low=mid+1;
+        else
+        high=mid-1;
+        }
+}
+
+
+       
+
+
