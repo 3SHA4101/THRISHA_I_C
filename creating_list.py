748791a6ad051d6b3602d152ce5984418a755583
3SHA4101 <thrisha4101@gmail.com>
2025-03-13 14:42:53 +0530

    Create creating_list.py

---
+++ creating_list.py
@@ -0,0 +1,8 @@
+n=int(input("how many elements in list u want"))
+mylist=[]
+for i in range(n):
+  a=int(input(f"enter {i} element"))
+  mylist.append(a)
+
+print("3sha 31")
+print("created list=",mylist)
